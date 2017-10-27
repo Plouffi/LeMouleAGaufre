efeff5b943e66a24a6c662665c77a11c853bcfc7
Unknown <remi.dumas17@hotmail.fr>
2017-10-27 09:02:45 +0200

    Architecture
    modele MVC

---
+++ Controler/sayHelloTo.ctrl.php
@@ -0,0 +1,37 @@
+<?php
+
+    //Include required files below (model, config...)
+
+    ///////////////////////////////////
+    //1st step: Get back data from view
+    ///////////////////////////////////
+
+    //We check if the have a name in the POST_ array (from the html form)
+    if(isset($POST_['name'])){
+        $name = $POST_['name'];
+    } else {
+        //If not, we create a variable error
+        $error = "Erreur: Le nom a été perdue dans la requête."
+    }
+
+    /////////////////////////
+    //2nd step: Handling data
+    /////////////////////////
+
+    if(!isset($error)) {
+        //This example doesnt to access the database
+        //So we ll skip this step
+    }
+
+    ////////////////////////////
+    //3rd step: Return the view
+    ////////////////////////////
+
+    //Depending on the data valeur, we include the corresponding view
+    if(isset($error)){
+        include("View/error.view.php");
+    }else{
+        //We create an assosiative array to gather data in a unique place
+        $data['name'] = $name;
+        include("View/mainExample.view.php");
+    }
