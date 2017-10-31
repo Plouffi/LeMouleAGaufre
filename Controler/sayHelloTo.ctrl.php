b0b96eb99c3c28e29b201011a86ab5923514e66c
Unknown <xgraouuuu17@gmail.com>
2017-10-31 13:44:29 +0100

    Erreur ?

--- Controler/sayHelloTo.ctrl.php
+++ Controler/sayHelloTo.ctrl.php
@@ -1,37 +1,38 @@
 <?php
 
-    //Include required files below (model, config...)
+//Include required files below (model, config...)
 
-    ///////////////////////////////////
-    //1st step: Get back data from view
-    ///////////////////////////////////
+///////////////////////////////////
+//1st step: Get back data from view
+///////////////////////////////////
 
-    //We check if the have a name in the POST_ array (from the html form)
-    if(isset($POST_['name'])){
-        $name = $POST_['name'];
-    } else {
-        //If not, we create a variable error
-        $error = "Erreur: Le nom a été perdue dans la requête."
-    }
+//We check if the have a name in the POST_ array (from the html form)
+if (isset($POST_['name'])) {
+    $name = $POST_['name'];
+} else {
+    //If not, we create a variable error
+    $error = "Erreur: Le nom a été perdue dans la requête.";
+}
 
-    /////////////////////////
-    //2nd step: Handling data
-    /////////////////////////
+/////////////////////////
+//2nd step: Handling data
+/////////////////////////
 
-    if(!isset($error)) {
-        //This example doesnt to access the database
-        //So we ll skip this step
-    }
+if (!isset($error)) {
+    //This example doesnt to access the database
+    //So we ll skip this step
+}
 
-    ////////////////////////////
-    //3rd step: Return the view
-    ////////////////////////////
+////////////////////////////
+//3rd step: Return the view
+////////////////////////////
 
-    //Depending on the data valeur, we include the corresponding view
-    if(isset($error)){
-        include("View/error.view.php");
-    }else{
-        //We create an assosiative array to gather data in a unique place
-        $data['name'] = $name;
-        include("View/mainExample.view.php");
-    }
+//Depending on the data valeur, we include the corresponding view
+if (isset($error)) {
+    include("View/error.view.php");
+} else {
+    //We create an assosiative array to gather data in a unique place
+    $data['name'] = $name;
+    include("View/mainExample.view.php");
+}
+?>
--- View/mainExample.view.php
+++ View/mainExample.view.php
@@ -1,17 +1,21 @@
 <!--Example-->
 <?php
-    include("header.view.php");
+include("header.view.php");
 ?>
-        <div id="main">
-            <h1 class="col-xs-offset-1 col-xs-9">Hello <?php (isset($data['name'])? echo $data['name']: echo 'world'; ?>!</h1>
-            <form method="POST" action="../Controler/sayHelloTo.ctrl.php">
-                <label class="label">Name:</label>
-                <input type="text" name="name" required>
-                <input type="submit">
-            </form>
-        </div>
+<div id="main">
+    <h1 class="col-xs-offset-1 col-xs-9">Hello <?php if (isset($data['name'])) {
+            echo $data['name'];
+        } else {
+            echo 'world';
+        } ?>!</h1>
+    <form method="POST" action="../Controler/sayHelloTo.ctrl.php">
+        <label class="label">Name:</label>
+        <input type="text" name="name" required>
+        <input type="submit">
+    </form>
+</div>
 
 
 <?php
-    include("footer.view.php");
+include("footer.view.php");
 ?>
