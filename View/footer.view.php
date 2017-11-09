efce4919c866b575cc2a21bd1915f31b57bb01ee
Unknown <julienfillon79@gmail.com>
2017-11-09 09:57:46 +0100

    compte

--- View/footer.view.php
+++ View/footer.view.php
@@ -1,5 +1,27 @@
-<footer>
+<html>
+    <head>
+    <title>Exemple</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../Ressources/css/MediaQueries/MediaQueries.css" rel="stylesheet" type="text/css" />
+        <link href="../Ressources/css/style.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    </head>
+    
+    <body>
+        
+    <!-- DEBUT de la page -->
+        
+        <footer>
+                <nav id="menu-footer">
+                        <ul>
+                            <li class="smenu"><a id="premier" href="#">Accueil</a></li>
+                            <li class="smenu"><a href="#">Quiz</a></li>
+                        </ul>
+                </nav>  
+                <h1>Site GreenIT Version © 2017</h1>
+        </footer>
 
-</footer>
-</body>
+    </body>
 </html>
